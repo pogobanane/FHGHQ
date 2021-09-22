81daae4f2d0d5650f204a619655f98cd9f258ae7
Julien Wolff <julien.wolff@bebeyond.de>
2021-09-22 09:42:56 +0200

    added maps.

--- warapi.js
+++ warapi.js
@@ -5,29 +5,38 @@ const db = require('./dbfunctions.js');
 const socket = require('./socket.js');
 const discordbot = require('./discordbot.js'); 
 var XMLHttpRequest = require('xhr2');
-const regionNames = ["DeadLandsHex",//3 
-                     "CallahansPassageHex",//4
-                      "MarbanHollow",//5
-                     "UmbralWildwoodHex",//6
-                     "MooringCountyHex",//7
-                     "HeartlandsHex", //8
-                     "LochMorHex",//9
-                     "LinnMercyHex",//10
-                     "ReachingTrailHex",//11
-                     "StonecradleHex",//12
-                     "FarranacCoastHex",//13
-                     "WestgateHex",//14
-                     "FishermansRowHex",//15
-                     "OarbreakerHex",//16                 
-                     "GreatMarchHex", //17
-                     "TempestIslandHex", //18
-                     "GodcroftsHex",//19
-                     "EndlessShoreHex",//20
-                     "AllodsBightHex",//21
-                     "WeatheredExpanseHex",//22
-                     "DrownedValeHex",//23
-                      "ShackledChasmHex",//24
-                      "ViperPitHex"] //25
+const regionNames = [
+    "DeadLandsHex",
+  "CallahansPassageHex",//4
+  "MarbanHollow",//5
+  "UmbralWildwoodHex",//6
+  "MooringCountyHex",//7
+  "HeartlandsHex", //8
+  "LochMorHex",//9
+  "LinnMercyHex",//10
+  "ReachingTrailHex",//11
+  "StonecradleHex",//12
+  "FarranacCoastHex",//13
+  "WestgateHex",//14
+  "FishermansRowHex",//15
+  "OarbreakerHex",//16
+  "GreatMarchHex", //17
+  "TempestIslandHex", //18
+  "GodcroftsHex",//19
+  "EndlessShoreHex",//20
+  "AllodsBightHex",//21
+  "WeatheredExpanseHex",//22
+  "DrownedValeHex",//23
+  "ShackledChasmHex",//24
+  "ViperPitHex",
+
+  "MorgensCrossingHex",
+  "OriginHex",
+    "AshFieldsHex",
+    "RedRiverHex",
+    "ClansheadValleyHex",
+    "HowlCountryHex"
+] //25
 
 //Pulling dynamic data from the WarAPI
 exports.updateMap= function (){
